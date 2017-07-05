d8871b8bf6e515c95ab2ce3d3f95214f2635b6d4
eppen <liuhualiang@gmail.com>
2017-07-05 14:46:51 +0800

    修改marker使用的图标策略

--- android/src/main/java/org/lovebing/reactnative/baidumap/MarkerUtil.java
+++ android/src/main/java/org/lovebing/reactnative/baidumap/MarkerUtil.java
@@ -23,14 +23,14 @@ public class MarkerUtil {
         LatLng position = getLatLngFromOption(option);
         maker.setPosition(position);
         maker.setTitle(option.getString("title"));
-	int icon = selectedImage(option);	
+	    int icon = selectedImage(option);
         BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(icon);
-	maker.setIcon(bitmap);
-	maker.setDraggable(false);
+	    maker.setIcon(bitmap);
+	    maker.setDraggable(false);
     }
 
     public static Marker addMarker(MapView mapView, ReadableMap option) {
-	int icon = selectedImage(option);	
+	    int icon = selectedImage(option);
         BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(icon);
         LatLng position = getLatLngFromOption(option);
         OverlayOptions overlayOptions = new MarkerOptions()
@@ -44,23 +44,25 @@ public class MarkerUtil {
     }
     
     private static int selectedImage(ReadableMap option) {
-	int icon;
-	int status = 1;
-	if(!option.hasKey("status")) {
-	    status = 1;
-	} else {
-	    status = option.getInt("status");
-	}
-	if(status == 0) {
-	    icon = R.mipmap.icon_bukezulin;
-	} else if(status == 1){
-	    icon = R.mipmap.icon_kezulin;
-	} else if(status == 2){
-	    icon = R.mipmap.icon_kezulin;
-	} else {
-	    icon = R.mipmap.icon_yijinzulin;
-	}
-	return icon;
+        int icon;
+        int status = 2;
+        if(!option.hasKey("status")) {
+            status = 2;
+        } else {
+            status = option.getInt("status");
+        }
+        if(status == 0) {
+            icon = R.mipmap.map_green;
+        } else if(status == 1){
+            icon = R.mipmap.map_or;
+        } else if(status == 2){
+            icon = R.mipmap.icon_gcoding;
+         } else if(status == 3){
+            icon = R.mipmap.map_water;
+        } else {
+            icon = R.mipmap.icon_gcoding;
+        }
+        return icon;
     }
 
     private static LatLng getLatLngFromOption(ReadableMap option) {
